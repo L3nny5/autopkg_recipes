2b78ca76fed2a1c12352458fa2fe02aa452e3cfb
Lennart Schwind <lennart.schwind@pcx0054.ad.aesthetics.mpg.de>
2018-10-24 16:26:05 +0200

    indention

--- Microsoft/OfficeSuiteSKULessVersionProvider.py
+++ Microsoft/OfficeSuiteSKULessVersionProvider.py
@@ -64,8 +64,8 @@ class OfficeSuiteSKULessVersionProvider(Processor):
         for vers in root.iter('latest'):
             package = vers.find('package')
             for pack in vers.iter('package'):
-            if pack.find('id').text == 'com.microsoft.office.suite.2016':
-                downurl = pack.find('download').text
+                if pack.find('id').text == 'com.microsoft.office.suite.2016':
+                    downurl = pack.find('download').text
         return downurl
 
     def main(self):
